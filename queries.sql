b3b9db6b39bae466be38b382ecb20bc741792e84
qwerus444-commits <qwerus444@gmail.com>
2026-02-23 18:11:01 +0300

    add SQL queries

---
+++ queries.sql
@@ -0,0 +1,15 @@
+
+SELECT c.login, COUNT(o.id) AS "delivery_count"
+FROM "Couriers" c
+LEFT JOIN "Orders" o ON c.id = o."courierId" AND o."inDelivery" = true
+GROUP BY c.login;
+
+
+SELECT track,
+       CASE
+           WHEN finished = true THEN 2
+           WHEN cancelled = true THEN -1
+           WHEN "inDelivery" = true THEN 1
+           ELSE 0
+       END AS status
+FROM "Orders";
